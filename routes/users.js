7698be67a6217e7ca3aebb7c5fbdda2e83e68483
Cheikh Bassirou GUEYE <bagueye905@gmail.com>
2025-07-12 08:40:37 +0000

    complements au niveau du dossier public

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,20 @@ router.get("/", (req, res) => {
 })
 
 router.get("/new", (req, res) => {
-    res.send("user new form")
+    res.render("users/new")
+})
+
+router.post("/", (req, res) => {
+    const isValid = false
+    if(isValid){
+        users.push({firstName: req.body.firstName})
+        res.redirect('/users/${users.length - 1}')
+    }else{
+        console.log("Error")
+        res.render('users/new', {firstName: req.body.firstName})
+    }
+
+    res.send("Hi")
 })
 
 router
